677fd08b86d9d9fac2f88455cdfcf183c8211d08
Ivan Dasilva <ivan.clate@gmail.com>
2015-08-03 12:12:51 +0200

    Añadida la validación de entradas durante la creación de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -24,13 +24,13 @@ exports.load = function(req, res, next, quizId) {
 // GET /quizes
 exports.index = function(req, res) {
   models.Quiz.findAll().then(function(quizes) {
-    res.render('quizes/index.ejs', { quizes: quizes});
+    res.render('quizes/index.ejs', { quizes: quizes, errors: []});
   })
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', {quiz: req.quiz})
+  res.render('quizes/show', {quiz: req.quiz, errors: []})
 };
 
 // GET /quizes/:id/answer
@@ -40,7 +40,7 @@ exports.answer = function(req, res) {
       resultado= 'Correcto'
     }
     res.render('quizes/answer', 
-               {quiz: req.quiz, respuesta: resultado});
+               {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 // GET /quizes/new
@@ -49,7 +49,7 @@ exports.new = function(req, res) {
    { pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
@@ -57,7 +57,15 @@ exports.create = function(req, res) {
   var quiz = models.Quiz.build(req.body.quiz);
 
   // guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  })  // Redirección HTTP (URL relativo) lista de preguntas
+  quiz.validate().then(
+    function(err) {
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz  // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then(function(){res.redirect('/quizes')})
+      }  // Redirección HTTP (URL relativo) lista de preguntas
+    }
+  );
 };
