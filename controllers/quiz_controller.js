47ef033327535b2e7529f826a0ff9bb234b3f8ea
Ivan Dasilva <ivan.clate@gmail.com>
2015-08-03 12:40:42 +0200

    Añadida la posibilidad de editar las preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,14 +18,16 @@ exports.load = function(req, res, next, quizId) {
         next(new Error('No existe quizId=' + quizId));
       }
     }
-  ).catch(function(error) { next(error); });
+  )
 };
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(function(quizes) {
-    res.render('quizes/index.ejs', { quizes: quizes, errors: []});
-  })
+  models.Quiz.findAll().then(
+    function(quizes) {
+      res.render('quizes/index.ejs', { quizes: quizes, errors: []});
+    }
+  ).catch(function(error){next(error)});
 };
 
 // GET /quizes/:id
@@ -63,6 +65,33 @@ exports.create = function(req, res) {
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
       } else {
         quiz  // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then(function(){res.redirect('/quizes')})
+      }  // Redirección HTTP (URL relativo) lista de preguntas
+    }
+  );
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz; // autoload de instancia de quiz
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(
+    function(err) {
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz  // save: guarda en DB campos pregunta y respuesta de quiz
         .save({fields: ["pregunta", "respuesta"]})
         .then(function(){res.redirect('/quizes')})
       }  // Redirección HTTP (URL relativo) lista de preguntas
